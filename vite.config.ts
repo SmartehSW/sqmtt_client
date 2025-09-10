96827dab8e66350061b92d1be8068fe3a4d5e76e
SmartehSW <jasa.crv@smarteh.si>
2025-09-10 09:45:46 +0200

    fixes to path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/SmartehMqtt/",
+    base: "/sqmtt_client/",
     define: {
         __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
     },
